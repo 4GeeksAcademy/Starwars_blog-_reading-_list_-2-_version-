0917e726cdab4ffa2b515604fad1c9eeed94a62b
Mariana <101410421+Placito@users.noreply.github.com>
2023-08-10 11:33:40 +0000

    trying to implement a search bar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,51 +2,92 @@ import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import logo from "../../img/Lego_Star_Wars_42.png";
-/*for the autocomplete
-let data = require("./Mock_DATA.json");*/
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
-	/*const [value, setValue] = useState('');
-	
+	const [input, setInput] = useState('');
+	const [autocompleteData, setAutocompleteData] = useState([]);
+	const [showAutocomplete, setShowAutocomplete] = useState(false); // New state for showing/hiding the autocomplete dropdown
 
-	//function to get the input value and put inside the state
-	const onChange = (e) => {
-		setValue(e.target.value);
+	const handleChange = (value) => {
+		setInput(value);
+		if (value.trim() === '') {
+			setAutocompleteData([]);
+			setShowAutocomplete(false); // Hide the autocomplete dropdown when input is empty
+		}
+
+		fetch(`https://www.swapi.tech/api/people?name=${value}`)
+		.then(response => {
+			if (!response.ok) {
+				throw Error(response.statusText);
+			}
+			return response.json(); // Parse the response JSON
+		})
+		.then(responseAsJson => {
+			console.log(responseAsJson)
+			
+			if (responseAsJson && responseAsJson.results && responseAsJson.results > 0) {
+				setAutocompleteData(responseAsJson.results.filter(result => result.name.toLowerCase().includes(value.toLowerCase())).map(result => result.name));
+				setShowAutocomplete(true); // Show the autocomplete dropdown when there are suggestions
+			}
+		})
+		.catch(error => {
+			console.log('Looks like there was a problem: \n', error);
+		});
 	}
 
-	// Get the value of a input when press the key Enter
-	const handleKeyDown = event => {
-		if (event.key === 'Enter') {
-			setValue(value.concat(value)) 
-			
-		// our api to fetch the search result
-		
-		*/
+	const handleSelectAutocomplete = (selectedItem) => {
+		setInput(selectedItem);
+		setShowAutocomplete(false); // Hide the autocomplete dropdown when a suggestion is selected
+	}
 
-	
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			// Your code to handle the "Enter" key press 
+			// let's just clear the input
+			setInput('');
+			setShowAutocomplete(false);
+		}
+	}
+
 	return (
 		<div className="container-fluid navbar-light bg-light">
 			<nav className="container navbar">
 				<Link to="/">
-						<img src={logo} style={{width: "100px"}} alt="star wars logo" />
+					<img src={logo} style={{ width: "100px" }} alt="star wars logo" />
 				</Link>
 				<div className="ml-auto">
-					{/*search bar with autocomplete*/}
-					<form className="form-inline mb-3">
-						<div className="input-group">
+					<form className="input-group mb-3">
 						<div className="input-group-prepend">
-							<span className="input-group-text" style={{height: "1.9rem"}} id="basic-addon1"><i className="fas fa-search"></i></span>
+							<div>
+								<span className="input-group-text" style={{ height: "1.9rem" }} id="basic-addon1"><i className="fas fa-search"></i></span>
+							</div>
 						</div>
-						{/*<input style={{height: "1.9rem"}} type="text" value={value} onChange={onChange} onKeyDown={handleKeyDown} className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"
-						/>
-						<div className="dropdown">
-							{data.map((item) => (
-								<div className="dropdown-row">{item}</div>
-							))}
-							</div>*/}
+						{/* Autocomplete dropdown */}
+						<div>
+							<input
+								style={{ height: "1.9rem" }}
+								type="text"
+								className="form-control"
+								value={input}
+								onChange={(e) => handleChange(e.target.value)}
+								placeholder="Search by name"
+								onKeyDown={handleKeyDown}
+							/>
+							{showAutocomplete && autocompleteData.length > 0 && (
+								<ul className="autocomplete-list">
+									{autocompleteData.map((item, index) => (
+										<li
+											key={index}
+											onClick={() => handleSelectAutocomplete(item)} // Handle selection
+										>
+											{item}
+										</li>
+									))}
+								</ul>
+							)}
 						</div>
-					</form>
+							</form>
 					<div className="dropdown">
 						<button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
 							Favorites <span className="badge bg-secondary">{store.favorites.length}</span>
